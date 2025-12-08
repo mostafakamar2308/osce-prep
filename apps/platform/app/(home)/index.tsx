1e590d956d154b75b38aaf902b576df34f77de78
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2025-12-08 23:08:17 +0200

    feat: updated platform theme

--- apps/platform/app/(home)/index.tsx
+++ apps/platform/app/(home)/index.tsx
@@ -1,9 +1,9 @@
-import Cases from '@/components/home/cases';
-import Header from '@/components/layout/Header';
-import SupportButton from '@/components/layout/SupportButton';
-import { Text } from '@/components/ui/text';
-import { useFormatMessage } from '@/hooks/intl';
-import { View } from 'react-native';
+import Cases from "@/components/home/cases";
+import Header from "@/components/layout/Header";
+import SupportButton from "@/components/layout/SupportButton";
+import { Text } from "@/components/ui/text";
+import { useFormatMessage } from "@/hooks/intl";
+import { View } from "react-native";
 
 export default function Index() {
   const intl = useFormatMessage();
@@ -11,8 +11,8 @@ export default function Index() {
   return (
     <View className="flex-1 relative flex gap-5 py-16 px-2">
       <Header />
-      <Text variant="h3" className="text-center text-chart-secondary">
-        {intl('home.verse')}{' '}
+      <Text className="text-center text-3xl text-primary">
+        {intl("home.verse")}{" "}
       </Text>
       <Cases />
       <SupportButton />
--- apps/platform/tailwind.config.js
+++ apps/platform/tailwind.config.js
@@ -1,10 +1,8 @@
-const { hairlineWidth } = require('nativewind/theme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
+  darkMode: ['class'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -41,38 +39,35 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        sidebar: {
+          DEFAULT: 'hsl(var(--sidebar))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          primary: 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          accent: 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))',
+        },
         chart: {
-          primary: 'hsl(var(--chart-1))',
-          secondary: 'hsl(var(--chart-2))',
-          tertiary: 'hsl(var(--chart-3))',
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
         },
       },
       borderRadius: {
+        xl: 'calc(var(--radius) + 4px)',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      borderWidth: {
-        hairline: hairlineWidth(),
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        serif: ['var(--font-serif)'],
+        mono: ['var(--font-mono)'],
       },
     },
   },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [require('tailwindcss-animate')],
 };
